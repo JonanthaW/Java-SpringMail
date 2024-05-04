ce65cbceddd2042b6ee85d92d2a979512b8706b3
JonanthaW <jonanthawillian@gmail.com>
2024-05-04 02:34:50 -0300

    Hello project

---
+++ src/main/java/com/JonanthaW/email/SpringBoot/Email.java
@@ -0,0 +1,4 @@
+package com.JonanthaW.email.SpringBoot;
+
+public record Email(String to, String subject, String body) {
+}
---
+++ src/test/java/com/JonanthaW/email/SpringBoot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JonanthaW.email.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
